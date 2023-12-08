f82741ec8d096e5c7d570131cb3c44f6d5aa9c1a
GeSamaras <georgiosamaras@hotmail.com>
2023-12-08 18:48:02 +0100

    delete, update and create fuctions

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,33 @@
+// {"userId":"123","noteId":"2f182ec0-958f-11ee-8764-5359c6a02564","content":"Hello World","attachment":"hello.jpg","createdAt":1702015269804}
+
+import * as uuid from "uuid";
+import { Table } from "sst/node/table";
+import handler from "@notes/core/handler";
+import dynamoDb from "@notes/core/dynamodb";
+
+export const main = handler(async (event) => {
+  let data = {
+    content: "",
+    attachment: "",
+  };
+
+  if (event.body != null) {
+    data = JSON.parse(event.body);
+  }
+
+  const params = {
+    TableName: Table.Notes.tableName,
+    Item: {
+      // The attributes of the item to be created
+      userId: "123", // The id of the author
+      noteId: uuid.v1(), // A unique uuid
+      content: data.content, // Parsed from request body
+      attachment: data.attachment, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  await dynamoDb.put(params);
+
+  return JSON.stringify(params.Item);
+});
